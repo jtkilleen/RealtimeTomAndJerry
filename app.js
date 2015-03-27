b29dc882575fbb44513804560a8a9fc7e2f7d748
Jeffrey Killeen <jtk12b@acu.edu>
2015-03-27 12:29:45 -0500

    Added sql capability

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var app = require('express')();
 var express = require('express');
+var mysql = require('mysql');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var pass = require('./password.js');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: pass.username(),
+	password: pass.password(),
+	database: 'news'
+});
+
+connection.connect();
+
+connection.query('SELECT * from accounts', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();
 
 app.use('/public', express.static(__dirname + "/public"));
 
@@ -12,7 +33,11 @@ var cat = {
 
     app.get('/', function (req, res) {
 	    res.sendFile(__dirname + '/index.html')
-	})
+	});
+
+	app.get('/animate', function (req, res) {
+	    res.sendFile(__dirname + '/animate.html')
+	});
 
 	io.on('connection', function(socket) {
 		socket.on('movement', function(msg){
